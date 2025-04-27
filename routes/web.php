34324728196219413a637c04af87517861e7187a
Joey J <joey@wyxos.com>
2025-04-27 10:23:01 +0400

    add task creation functionality: implement Create.vue component and update routing

--- routes/web.php
+++ routes/web.php
@@ -38,6 +38,8 @@ Route::middleware(['auth', 'verified'])->group(function () {
     // tasks
     Route::get('tasks', [TaskController::class, 'index'])->name('tasks.index');
 
+    Route::get('tasks/create', [TaskController::class, 'create'])->name('tasks.create');
+
     Route::post('tasks', [TaskController::class, 'store'])->name('tasks.store');
 
     Route::put('tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
