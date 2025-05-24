fca75345b2243dd45e6e6dd9459da6b8b3cc4397
Joey J <github@wyxos.com>
2025-05-24 12:53:32 +0400

    feat: implement ExternalTaskController and update task routes for external access

--- routes/api.php
+++ routes/api.php
@@ -9,11 +9,11 @@ Route::get('/user', function (Request $request) {
 
 Route::middleware('auth:sanctum')->group(function () {
     // tasks
-    Route::get('/tasks', [\App\Http\Controllers\Api\TaskController::class, 'index'])->name('tasks.index');
-    Route::get('/tasks/{task}', [\App\Http\Controllers\Api\TaskController::class, 'show'])->name('tasks.show');
-    Route::post('/tasks', [\App\Http\Controllers\Api\TaskController::class, 'store'])->name('tasks.store');
-    Route::put('/tasks/{task}', [\App\Http\Controllers\Api\TaskController::class, 'update'])->name('tasks.update');
-    Route::delete('/tasks/{task}', [\App\Http\Controllers\Api\TaskController::class, 'destroy'])->name('tasks.destroy');
-    Route::patch('/tasks/{task}/toggle-status', [\App\Http\Controllers\Api\TaskController::class, 'toggleStatus'])->name('tasks.toggle-status');
-    Route::patch('/tasks/{task}/toggle-priority', [\App\Http\Controllers\Api\TaskController::class, 'togglePriority'])->name('tasks.toggle-priority');
+    Route::get('/tasks', [\App\Http\Controllers\Api\ExternalTaskController::class, 'index'])->name('tasks.index');
+    Route::get('/tasks/{task}', [\App\Http\Controllers\Api\ExternalTaskController::class, 'show'])->name('tasks.show');
+    Route::post('/tasks', [\App\Http\Controllers\Api\ExternalTaskController::class, 'store'])->name('tasks.store');
+    Route::put('/tasks/{task}', [\App\Http\Controllers\Api\ExternalTaskController::class, 'update'])->name('tasks.update');
+    Route::delete('/tasks/{task}', [\App\Http\Controllers\Api\ExternalTaskController::class, 'destroy'])->name('tasks.destroy');
+    Route::patch('/tasks/{task}/toggle-status', [\App\Http\Controllers\Api\ExternalTaskController::class, 'toggleStatus'])->name('tasks.toggle-status');
+    Route::patch('/tasks/{task}/toggle-priority', [\App\Http\Controllers\Api\ExternalTaskController::class, 'togglePriority'])->name('tasks.toggle-priority');
 });
