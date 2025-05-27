0d33a34bfe66e4816642f0d2e2caff8d82f0473a
Joey J <github@wyxos.com>
2025-05-27 14:08:00 +0400

    feat: notify project owner upon user registration completion

---
+++ app/Notifications/ProjectUserRegisteredNotification.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Project;
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ProjectUserRegisteredNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * The user who registered.
+     *
+     * @var \App\Models\User
+     */
+    protected $registeredUser;
+
+    /**
+     * The project instance.
+     *
+     * @var \App\Models\Project
+     */
+    protected $project;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(User $registeredUser, Project $project)
+    {
+        $this->registeredUser = $registeredUser;
+        $this->project = $project;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->subject('New User Registration in Your Project')
+            ->line('A new user has completed registration for your project: ' . $this->project->name)
+            ->line('User: ' . $this->registeredUser->name)
+            ->line('Email: ' . $this->registeredUser->email)
+            ->action('View Project', url('/projects/' . $this->project->id))
+            ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            'project_id' => $this->project->id,
+            'project_name' => $this->project->name,
+            'user_id' => $this->registeredUser->id,
+            'user_name' => $this->registeredUser->name,
+            'user_email' => $this->registeredUser->email,
+        ];
+    }
+}
