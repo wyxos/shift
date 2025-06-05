41c286d9ccea17a7fec7f633ec6f818e1a101a3f
Wyxos <github@wyxos.com>
2025-06-05 05:09:30 +0400

    Add basic dashboard metrics

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,7 @@ Route::get('/', function () {
 
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard');
+    Route::get('dashboard', [\App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
 
     // organisations
     Route::get('organisations', [OrganisationController::class, 'index'])->name('organisations.index');
