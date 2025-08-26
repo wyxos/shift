0f6a3b3d67f4e22040ba2d0ede09af2adfe10fb1
Joey J <github@wyxos.com>
2025-08-26 21:54:16 +0400

    feat: add breadcrumbs and layout structure to Components.vue

--- resources/js/pages/Components.vue
+++ resources/js/pages/Components.vue
@@ -1,5 +1,20 @@
 <script setup lang="ts">
+import AppLayout from '@/layouts/AppLayout.vue';
+import { type BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/vue3';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Dashboard',
+        href: '/dashboard',
+    },
+];
 </script>
 
 <template>
+    <Head title="Dashboard" />
+
+    <AppLayout :breadcrumbs="breadcrumbs">
+        <div class="p-4">TEst</div>
+    </AppLayout>
 </template>
