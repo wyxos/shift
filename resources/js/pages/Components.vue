8abbd83b4838750f407c3f2647956b2eeb9984cc
Joey J <joey@wyxos.com>
2025-08-26 23:54:58 +0400

    feat: integrate TipTap editor into Components.vue with basic test

--- resources/js/pages/Components.vue
+++ resources/js/pages/Components.vue
@@ -1,20 +1,27 @@
 <script setup lang="ts">
-import AppLayout from '@/layouts/AppLayout.vue';
-import { type BreadcrumbItem } from '@/types';
-import { Head } from '@inertiajs/vue3';
+import AppLayout from '@/layouts/AppLayout.vue'
+import { type BreadcrumbItem } from '@/types'
+import { Head } from '@inertiajs/vue3'
+import { useEditor, EditorContent } from '@tiptap/vue-3'
+import StarterKit from '@tiptap/starter-kit'
 
 const breadcrumbs: BreadcrumbItem[] = [
-    {
-        title: 'Dashboard',
-        href: '/dashboard',
-    },
-];
+  { title: 'Components', href: '/components' },
+]
+
+const editor = useEditor({
+  extensions: [StarterKit],
+  content: '<p>Hello TipTap</p>',
+})
 </script>
 
 <template>
-    <Head title="Dashboard" />
-
-    <AppLayout :breadcrumbs="breadcrumbs">
-        <div class="p-4">TEst</div>
-    </AppLayout>
+  <Head title="Components" />
+  <AppLayout :breadcrumbs="breadcrumbs">
+    <div class="p-4">
+      <div class="rounded border p-3">
+        <EditorContent data-testid="tiptap-editor" :editor="editor" />
+      </div>
+    </div>
+  </AppLayout>
 </template>
