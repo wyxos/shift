c22117fb2a91e907ac3391c7d50f06622f2bf07e
Wyxos <wyxos@outlook.com>
2026-02-03 22:39:42 +0400

    Improve inline image upload UI

---
+++ resources/js/components/InlineImageNodeView.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { NodeViewWrapper } from '@tiptap/vue-3'
+import { X } from 'lucide-vue-next'
+
+const props = defineProps<{
+  node: { attrs: Record<string, any> }
+  deleteNode: () => void
+}>()
+
+const remove = () => {
+  props.deleteNode()
+}
+</script>
+
+<template>
+  <NodeViewWrapper as="span" class="relative inline-block align-middle group">
+    <img
+      class="editor-tile"
+      :src="props.node.attrs.src"
+      :alt="props.node.attrs.alt || ''"
+      :title="props.node.attrs.title || ''"
+    />
+    <button
+      type="button"
+      contenteditable="false"
+      class="absolute right-2 top-2 inline-flex h-6 w-6 cursor-pointer items-center justify-center rounded-md bg-red-50 text-red-600 shadow-sm ring-1 ring-red-200 transition hover:bg-red-100 hover:text-red-700 hover:ring-red-300 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-red-400"
+      @click="remove"
+    >
+      <X :size="14" />
+    </button>
+  </NodeViewWrapper>
+</template>
