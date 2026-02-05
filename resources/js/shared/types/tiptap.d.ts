abf42bd07e4ae400e6573d04a2ddeea5af923a7f
Wyxos <wyxos@outlook.com>
2026-02-05 14:36:18 +0400

    feat: implement chunked file upload for images with progress tracking and inline image support

---
+++ resources/js/shared/types/tiptap.d.ts
@@ -0,0 +1,10 @@
+import '@tiptap/core'
+
+declare module '@tiptap/core' {
+  interface Commands<ReturnType> {
+    imageUpload: {
+      insertFiles: (files: File[]) => ReturnType
+      typeText: (text: string) => ReturnType
+    }
+  }
+}
