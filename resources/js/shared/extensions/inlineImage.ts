36c032c8de631d3ddb98b1d8e2d8ff84af2ba13e
Wyxos <github@wyxos.com>
2026-02-05 14:36:18 +0400

    feat: implement chunked file upload for images with progress tracking and inline image support

---
+++ resources/js/shared/extensions/inlineImage.ts
@@ -0,0 +1,11 @@
+import TiptapImage from '@tiptap/extension-image'
+import { VueNodeViewRenderer } from '@tiptap/vue-3'
+import InlineImageNodeView from '../components/InlineImageNodeView.vue'
+
+const InlineImage = TiptapImage.extend({
+  addNodeView() {
+    return VueNodeViewRenderer(InlineImageNodeView as any)
+  },
+})
+
+export default InlineImage
