9b995661ddb5672890334b214c9bcf364a778886
Joey J <github@wyxos.com>
2026-07-21 14:56:11 +0400

    fix: clean temporary attachments and chunks safely

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,20 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command('attachments:clean-temp')
+    ->daily()
+    ->timezone(config('app.timezone'))
+    ->withoutOverlapping(60)
+    ->onOneServer();
+
+Schedule::command('tasks:notify-awaiting-feedback')
+    ->daily()
+    ->timezone(config('app.timezone'))
+    ->withoutOverlapping(60)
+    ->onOneServer();
