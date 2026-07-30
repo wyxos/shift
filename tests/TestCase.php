6d1f43dcb0ac36bf42e421b83e12b0c3e7bacc81
Joey J <github@wyxos.com>
2026-07-30 10:17:57 +0400

    test: make callback DNS resolution deterministic

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,20 @@
 
 namespace Tests;
 
+use App\Services\OutboundUrlPolicy;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->app->bind(
+            OutboundUrlPolicy::class,
+            fn (): OutboundUrlPolicy => new OutboundUrlPolicy(
+                fn (): array => ['93.184.216.34'],
+            ),
+        );
+    }
 }
